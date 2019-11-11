e9d5274ab5cdb56056aedddbe4044c95c496bca3
Brian Zhu <brianzhu0512@gmail.com>
2019-11-11 03:32:22 -0500

    Fixed Homescreen CSS and fixed most of Listscreen CSS.

--- src/components/list_screen/ListScreen.js
+++ src/components/list_screen/ListScreen.js
@@ -33,17 +33,42 @@ class ListScreen extends Component {
         }
 
         return (
-            <div className="container white">
-                <h5 className="grey-text text-darken-3">Todo List</h5>
+            <div className="container todo-list">
+                <br />
+                <div className="list-heading-container">
+                    <h5 className="grey-text text-darken-3 list-heading">Todo List</h5>
+                    <div className="list-trash">&#x1f5d1;</div>
+                </div>
+                <br />
+                <br />
+                <br />
                 <div className="input-field">
-                    <label htmlFor="email">Name</label>
+                    <label htmlFor="email">Name:</label>
                     <input className="active" type="text" name="name" id="name" onChange={this.handleChange} value={todoList.name} />
                 </div>
                 <div className="input-field">
-                    <label htmlFor="password">Owner</label>
+                    <label htmlFor="password">Owner:</label>
                     <input className="active" type="text" name="owner" id="owner" onChange={this.handleChange} value={todoList.owner} />
                 </div>
-                <ItemsList todoList={todoList} />
+                <div id="list-items-container">
+                    <div className="list_item_header_card">
+                        <div className="list_item_task_header">
+                            Task
+                        </div>
+                        <div className="list_item_due_date_header">
+                            Due Date
+                        </div>
+                        <div className="list_item_status_header">
+                            Status
+                        </div>
+                    </div>
+
+                    <ItemsList todoList={todoList} />
+                    <div className = "list_item_add_card center-align">
+                        +
+                    </div>
+
+                </div> 
             </div>
         );
     }
@@ -53,7 +78,6 @@ const mapStateToProps = (state, ownProps) => {
   const { id } = ownProps.match.params;
   const { todoLists } = state.firestore.data;
   const todoList = todoLists ? todoLists[id] : null;
-  todoList.id = id;
 
   if(todoList)
   {
